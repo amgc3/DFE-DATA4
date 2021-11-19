24b20e945321dfd13d2626d920da46247e45b8e8
Anna <adropofwater0@gmail.com>
2021-11-19 15:13:37 +0000

    create an output file with all uppercase characters

--- filerw/readfile2.py
+++ filerw/readfile2.py
@@ -1,5 +1,9 @@
- fin = open('dog_breeds.txt')
+fin = open('dog_breeds.txt')
 # use file object as part of for loop
 # => we can iterate directly on fin without calling readlines()
+fout = open('allupper.txt', 'w')
 for line in fin:
-    print(line.strip())
+    print(line.strip().upper())
+    fout.write(line.upper())
+fout.close()
+fin.close()
